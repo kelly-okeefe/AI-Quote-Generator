ffeca0fb60a088655d732b1b0339f65121691bb1
kellymokeefe <128492828+kellymokeefe@users.noreply.github.com>
2023-12-23 14:33:44 +0100

    Updated font, and changed from grid style.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+
 function displayPoem(response) {
-  console.log("poem generated");
+  let mainImage = document.querySelector("img");
+  mainImage.classList.add("hidden");
+
     new Typewriter('#poem', {
         strings: response.data.answer,
         autoStart: true,
@@ -12,24 +15,13 @@ function displayPoem(response) {
 
 function generatePoem(event) {
     event.preventDefault();
-
-    new Typewriter('#poem', {
-        strings: "I am to wait, though waiting be so hell... Time is very slow for those who wait...",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-        delay: 50,
-      });
-
+    let poemElement=document.querySelector("#poem");
+    poemElement.innerHTML="Poem generating..."
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "e7f44dtf8936b0ao9a003f375cfb3403";
     let context = "You are a Shakespeare expert, who loves to write poetry. Your mission is to generate a poem, no more than six lines, in basic HTML and separate each line with a </br>. The poem should sound like it was written by William Shakespeare. Make sure to follow the user instructions. Do not include a title to the poem.";
     let prompt = `User instructions: Please generate a poem about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-    console.log("Generating poem");
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
 
     axios.get(apiUrl).then(displayPoem);
 }
